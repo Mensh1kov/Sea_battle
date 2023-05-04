ffc63b8f4688f1539fde25629d6fb5ac73138e63
Mensh1kov <www.sasha.menshikov018@mail.ru>
2023-05-04 23:28:07 +0500

    Изменена таблица кораблей. Убрана колонка "Count". Добавлена возможность получения корабля по номеру строки

--- new_arch/views/widgets/table_ships_widget.py
+++ new_arch/views/widgets/table_ships_widget.py
@@ -4,22 +4,26 @@ from pyqt5_plugins.examplebutton import QtWidgets
 
 class TableShipsWidget(QTableWidget):
     def __init__(self, widget: QWidget = None):
-        super().__init__(0, 3, widget)
+        super().__init__(0, 2, widget)
 
-        self.horizontalHeader().setDefaultSectionSize(80)
+        self.horizontalHeader().setDefaultSectionSize(119)
         self.horizontalHeader().setMinimumSectionSize(0)
         self.verticalHeader().setDefaultSectionSize(23)
         self.verticalHeader().setMinimumSectionSize(0)
-        self.setHorizontalHeaderLabels(['Width', 'Height', 'Count'])
+        self.setHorizontalHeaderLabels(['Width', 'Length'])
 
-    def get_list_ships(self) -> list:
-        rows = []
+    def get_list_ships(self) -> list[(int, int)]:
+        ships = []
         for row in range(self.rowCount()):
+            ships.append(self.get_ship(row))
+        return ships
+
+    def get_ship(self, row: int) -> (int, int):
+        if row < self.rowCount():
             row_data = []
             for column in range(self.columnCount()):
                 row_data.append(int(self.item(row, column).text()))
-            rows.append(tuple(row_data))
-        return rows
+            return tuple(row_data)
 
     def add_ship(self):
         self.insertRow(0)
@@ -27,7 +31,8 @@ class TableShipsWidget(QTableWidget):
     def remove_ship(self):
         self.removeRow(self.rowCount() - 1)
 
-    def set_ships(self, ships: list[tuple[int, int, int]]):
+    def set_ships(self, ships: list[(int, int)]):
+        self.setRowCount(0)
         for row in range(len(ships)):
             self.insertRow(row)
             for column, value in enumerate(ships[row]):
@@ -38,5 +43,6 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     game_part = TableShipsWidget()
+    game_part.set_ships([(1, 2), (2, 3)])
     game_part.show()
     sys.exit(app.exec_())
